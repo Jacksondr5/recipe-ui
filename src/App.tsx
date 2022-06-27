483c0a25af8361bd08ed8f25f1d409caf7cea453
Abraham Oh <abeoh1@gmail.com>
2022-06-27 10:22:14 -0400

    Images
    added basic image support onto the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import "antd/dist/antd.css";
-import './App.css';
-import { Recipe } from './recipe';
-import { Collapse, Select } from 'antd';
+import "./App.css";
+import { Recipe } from "./recipe";
+import { Collapse, Select, Image } from "antd";
 import { EditOutlined } from "@ant-design/icons";
 
 function App() {
   const [page, setPage] = useState<Recipe[]>();
 
   const getAllRecipes = async () => {
-    const response = await fetch('http://localhost:3001/recipe');
+    const response = await fetch("http://localhost:3001/recipe");
     const data: Recipe[] = await response.json();
     console.log(data);
     setPage(data);
-  }
+  };
 
   useEffect(() => {
-    getAllRecipes()
-    console.log(page)
+    getAllRecipes();
+    console.log(page);
   }, []);
 
   const onChange = (key: string | string[]) => {
@@ -27,42 +27,50 @@ function App() {
   const { Panel } = Collapse;
   const { Option } = Select;
 
-  const genExtra = () => (
-    <EditOutlined
-      onClick={(event) => {
-        // If you don't want click extra trigger collapse, you can prevent this:
-        event.stopPropagation();
-      }}
-    />
+  const genExtra = (thumbnail: string) => (
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <Image width={150} src={thumbnail} />
+      <EditOutlined
+        onClick={(event) => {
+          // If you don't want click extra trigger collapse, you can prevent this:
+          event.stopPropagation();
+        }}
+      />
+    </div>
   );
+
+  const showPicture = (thumbnail: string) => {
+    if (thumbnail !== "") {
+      return (
+        <>
+          <Image width={150} src={thumbnail} />
+        </>
+      );
+    }
+  };
 
   return (
     <div className="App">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "50px auto",
-        }}
-      >
-        <h1>Recipe App</h1>
-      </div>
+      <h1>Recipe App</h1>
 
       <div className="Ipad">
-        {page?.map((item, index) =>
-          <div className="Recipes" key = {index}> 
-            <Collapse 
-              onChange={onChange}
-              expandIconPosition='end'
-            >
-              <Panel header={item.name} key={index} extra={genExtra()}>
-                <div>{item.description}</div>
-              </Panel>
-            </Collapse>
-            <br/>
+        {page?.map((item, index) => (
+          <div className="Recipes" key={index}>
+            <>{showPicture(item.thumbnail)}</>
+            <div className="RecipeBox">
+              <Collapse onChange={onChange} expandIconPosition="end">
+                <Panel
+                  header={item.name}
+                  key={index}
+                  extra={genExtra(item.thumbnail)}
+                >
+                  <div>{item.description}</div>
+                </Panel>
+              </Collapse>
+            </div>
+            <br />
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
