d2c2730680e151ee89af6a56b8d70ccbf0304637
Abraham Oh <abeoh1@gmail.com>
2022-07-06 10:02:08 -0400

    PR changes
    add .env.example
    
    changed url env to include the route
    
    changed null checks to be more inclusive
    
    added more descriptive error messages
    
    renamed certain variables
    
    shifted metadata box next to description box
    
    bolded and increased recipe name in header

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,27 @@ import { Button, Collapse, Image } from "antd";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 function App() {
-  const url = `${process.env.REACT_APP_URL}`;
-  if (url === undefined) {
-    throw new Error("invalid URL");
+  const baseUrl = `${process.env.REACT_APP_API_URL}`;
+  if (!baseUrl) {
+    throw new Error("The URL environment variable is undefined/missing");
   }
+  const url = baseUrl + "/recipe";
   const [allRecipes, setAllRecipes] = useState<Recipe[]>([]);
 
   const getAllRecipes = async () => {
     const response = await fetch(url);
     const data: Recipe[] = await response.json();
-    if (data === undefined) {
-      throw new Error("bad api call");
-    } else {
-      data.map((recipe) => {
-        if (recipe.thumbnail.image === "") {
-          recipe.thumbnail.show = false;
-        }
-      });
-      setAllRecipes(data);
+    if (!data) {
+      throw new Error(
+        "The fetch call did not return any data matching the Recipe Type"
+      );
     }
+    data.map((recipe) => {
+      if (recipe.thumbnail.image === "") {
+        recipe.thumbnail.show = false;
+      }
+    });
+    setAllRecipes(data);
   };
 
   useEffect(() => {
@@ -66,14 +68,18 @@ function App() {
   );
 
   const getRecommendationPic = (id: string) => {
-    var recommendation = allRecipes.find((item) => item.id === parseInt(id));
+    var recommendation = allRecipes.find(
+      (recipe) => recipe.id === parseInt(id)
+    );
     if (recommendation !== undefined) {
       return recommendation.thumbnail.image;
     }
   };
 
   const getRecommendationName = (id: string) => {
-    var recommendation = allRecipes.find((item) => item.id === parseInt(id));
+    var recommendation = allRecipes.find(
+      (recipe) => recipe.id === parseInt(id)
+    );
     if (recommendation !== undefined) {
       return recommendation.name;
     }
@@ -90,47 +96,63 @@ function App() {
       <h1>Recipe App</h1>
 
       <div className="Ipad">
-        {allRecipes.map((item, index) => (
+        {allRecipes.map((recipe, index) => (
           <div className="Recipes" key={index}>
             <>
-              {item.thumbnail.show && (
-                <Image width={150} src={item.thumbnail.image} preview={false} />
+              {recipe.thumbnail.show && (
+                <Image width={150} src={recipe.thumbnail.image} />
               )}
             </>
             <div className="RecipeBox">
               <Collapse
                 onChange={(e) =>
-                  onChange(e, index, item.thumbnail.image, item.thumbnail.show)
+                  onChange(
+                    e,
+                    index,
+                    recipe.thumbnail.image,
+                    recipe.thumbnail.show
+                  )
                 }
                 expandIconPosition="end"
               >
-                <Panel header={item.name} key={index} extra={genExtra()}>
-                  <div className="DescriptionBox">{item.description}</div>
-                  <div className="FirstHalf">
-                    <div className="IngredientBox">
-                      <h2>Ingredients</h2>
-                      {item.ingredients.map((ingred, ingredIndex) => (
-                        <div key={ingredIndex}>
-                          {displayIngredient(ingred.ingredient, ingred.starred)}
-                        </div>
-                      ))}
-                    </div>
+                <Panel
+                  header={<b style={{ fontSize: "20px" }}>{recipe.name}</b>}
+                  key={index}
+                  extra={genExtra()}
+                >
+                  <div className="FirstSection">
+                    <div className="DescriptionBox">{recipe.description}</div>
                     <div className="MetadataBox">
-                      <p>Created: {item.metadata.created}</p>
-                      <p>Last Viewed: {item.metadata.lastViewed}</p>
-                      <p>Time to Cook: {item.metadata.timeToCook}</p>
+                      <p>Created: {recipe.metadata.created}</p>
+                      <p>Last Viewed: {recipe.metadata.lastViewed}</p>
+                      <p>Time to Cook: {recipe.metadata.timeToCook}</p>
                     </div>
+                  </div>
+                  <div className="IngredientBox">
+                    <h2>Key Ingredients</h2>
+                    {recipe.ingredients.map((ingredient, ingredIndex) => (
+                      <div key={ingredIndex}>
+                        {displayIngredient(
+                          ingredient.ingredient,
+                          ingredient.starred
+                        )}
+                      </div>
+                    ))}
                   </div>
                   <div className="RecommendationTitle">
                     <h2>Recommendations</h2>
                   </div>
                   <div className="RecommendationsBox">
-                    {item.link.map((rec, recIndex) => (
+                    {recipe.link.map((linkedRecipeId, recIndex) => (
                       <div className="RecommendationCard" key={recIndex}>
                         <div>
-                          <Image width={100} src={getRecommendationPic(rec)} />
+                          <Image
+                            width={100}
+                            src={getRecommendationPic(linkedRecipeId)}
+                            preview={false}
+                          />
                         </div>
-                        <p>{getRecommendationName(rec)}</p>
+                        <p>{getRecommendationName(linkedRecipeId)}</p>
                       </div>
                     ))}
                   </div>
