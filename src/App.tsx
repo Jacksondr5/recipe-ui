50a0a29c9316032b9821298fec00fe9f66831c1c
Abraham Oh <abeoh1@gmail.com>
2022-07-09 20:04:28 -0400

    Homepage (#1)
    * basic fetch
    
    Added a function to fetch all recipes
    
    * Basic Home Page Setup
    
    UI now properly pulls data from API
    
    Setup base template to generate antd collapse content from the API.
    
    * Images
    
    added basic image support onto the home page
    
    * dynamic image attempt
    
    thumbnail properly disappears when expanding panel
    
    thumbnail does not reappear when collapsing panel
    
    * expanded recipe card template
    
    created base template for a recipe card on the homepage once expanded
    
    * Images fixed
    
    Thumbnail image properly hides and shows depending on the expansion of the recipe card
    
    Updated the Recipe interface for better state tracking
    
    Recommendations properly show a picture of the recipe now
    
    * ingredient update
    
    updated ingredients array to include a "starred" option
    
    updated the ingredients box to only show starred ingredients
    
    * code cleanup
    
    removed commented lines and functions
    
    * pr changes
    
    removed borders around divs
    
    updated api fetch url to an environment variable
    
    added stylized borders in recipe card for additional readability
    
    removed repeated undefined checks
    
    renamed state variable
    
    * PR changes
    
    add .env.example
    
    changed url env to include the route
    
    changed null checks to be more inclusive
    
    added more descriptive error messages
    
    renamed certain variables
    
    shifted metadata box next to description box
    
    bolded and increased recipe name in header
    
    * more pr updates
    
    renamed css classes
    
    updated env to catch empty env
    
    removed unnecessary function params
    
    removed title bar
    
    removed redundant functions
    
    * interface update
    
    changed an interface export name to singular
    
    * interface update
    
    Removed a property from the recipe interface that doesn't get updated in the database
    
    intersected a new interface to house the show property

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,174 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "antd/dist/antd.min.css";
+import "./App.css";
+import { Recipe } from "./recipe";
+import { Button, Collapse, Image } from "antd";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 
 function App() {
+  type uiRecipe = Recipe & {
+    thumbnail: {
+      show: boolean;
+    };
+  };
+
+  const baseUrl = `${process.env.REACT_APP_API_URL}`;
+  if (!baseUrl || baseUrl === "undefined") {
+    throw new Error("The URL environment variable is undefined/missing");
+  }
+
+  const url = baseUrl + "/recipe";
+  const [allRecipes, setAllRecipes] = useState<uiRecipe[]>([]);
+
+  const getAllRecipes = async () => {
+    const response = await fetch(url);
+    const data: uiRecipe[] = await response.json();
+    if (!data) {
+      throw new Error(
+        "The fetch call did not return any data matching the Recipe Type"
+      );
+    }
+    data.map((recipe) => {
+      if (recipe.thumbnail.image === "") {
+        recipe.thumbnail.show = false;
+      }
+      recipe.thumbnail.show = true;
+    });
+    setAllRecipes(data);
+  };
+
+  useEffect(() => {
+    getAllRecipes();
+  }, []);
+
+  const onChange = (index: number) => {
+    const tempPage: uiRecipe[] = [...allRecipes];
+    const recipe = { ...tempPage[index] };
+    if (recipe.thumbnail.image !== "") {
+      if (recipe.thumbnail.show === true) {
+        recipe.thumbnail.show = false;
+      } else {
+        recipe.thumbnail.show = true;
+      }
+      tempPage[index] = recipe;
+      setAllRecipes(tempPage);
+    } else {
+      recipe.thumbnail.show = false;
+      tempPage[index] = recipe;
+      setAllRecipes(tempPage);
+    }
+  };
+
+  const { Panel } = Collapse;
+
+  const genExtra = () => (
+    <div className="App-Extra">
+      <EditOutlined
+        onClick={(event) => {
+          // If you don't want click extra trigger collapse, you can prevent this:
+          event.stopPropagation();
+        }}
+      />
+    </div>
+  );
+
+  const getRecommendationPic = (id: string) => {
+    var recommendation = allRecipes.find(
+      (recipe) => recipe.id === parseInt(id)
+    );
+    if (recommendation !== undefined) {
+      return recommendation.thumbnail.image;
+    }
+  };
+
+  const getRecommendationName = (id: string) => {
+    var recommendation = allRecipes.find(
+      (recipe) => recipe.id === parseInt(id)
+    );
+    if (recommendation !== undefined) {
+      return recommendation.name;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Ipad">
+        {allRecipes.map((recipe, index) => (
+          <div className="App-Recipes" key={index}>
+            <>
+              {recipe.thumbnail.show && (
+                <Image width={150} src={recipe.thumbnail.image} />
+              )}
+            </>
+            <div className="App-RecipeBox">
+              <Collapse
+                onChange={() => onChange(index)}
+                expandIconPosition="end"
+              >
+                <Panel
+                  header={
+                    <div>
+                      <b style={{ fontSize: "20px", padding: "15px" }}>
+                        {recipe.name}
+                      </b>
+                    </div>
+                  }
+                  key={index}
+                  extra={genExtra()}
+                >
+                  <div className="App-FirstSection">
+                    <div className="App-DescriptionBox">
+                      {recipe.description}
+                    </div>
+                    <div className="App-MetadataBox">
+                      <p>Created: {recipe.metadata.created}</p>
+                      <p>Last Viewed: {recipe.metadata.lastViewed}</p>
+                      <p>Time to Cook: {recipe.metadata.timeToCook}</p>
+                    </div>
+                  </div>
+                  <div className="App-IngredientBox">
+                    <h2>Key Ingredients</h2>
+                    {recipe.ingredients.map((ingredient, ingredIndex) => (
+                      <div key={ingredIndex}>
+                        {ingredient.starred && ingredient.ingredient}
+                      </div>
+                    ))}
+                  </div>
+                  <div className="App-RecommendationTitle">
+                    <h2>Recommendations</h2>
+                  </div>
+                  <div className="App-RecommendationsBox">
+                    {recipe.link.map((linkedRecipeId, recIndex) => (
+                      <div className="App-RecommendationCard" key={recIndex}>
+                        <div>
+                          <Image
+                            width={100}
+                            src={getRecommendationPic(linkedRecipeId)}
+                            preview={false}
+                          />
+                        </div>
+                        <p>{getRecommendationName(linkedRecipeId)}</p>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="App-ButtonBox">
+                    <div className="App-ReadingButton">
+                      <Button>Reading Mode</Button>
+                    </div>
+                    <div className="App-CookingButton">
+                      <Button>Cooking Mode</Button>
+                    </div>
+                  </div>
+                  <div className="App-DeleteButton">
+                    <Button danger icon={<DeleteOutlined />}></Button>
+                  </div>
+                </Panel>
+              </Collapse>
+            </div>
+            <br />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/recipe.ts
@@ -0,0 +1,30 @@
+export interface Recipe {
+  id: number;
+  name: string;
+  thumbnail: Thumbnail;
+  description: string;
+  link: string[];
+  metadata: Metadata;
+  ingredients: Ingredient[];
+  steps: Steps[];
+}
+
+export interface Thumbnail {
+  image: string;
+}
+
+export interface Ingredient {
+  ingredient: string;
+  starred: boolean;
+}
+
+export interface Metadata {
+  lastViewed: string;
+  created: string;
+  timeToCook: string;
+}
+
+export interface Steps {
+  step: number;
+  directions: string;
+}
